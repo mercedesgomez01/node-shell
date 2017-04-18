bec8c7bc309f68beca818486a41a06de33c35744
mercedesgomez01 <mercedesgomez01@gmail.com>
2017-04-18 13:02:30 -0400

    now using fs file system

--- commands.js
+++ commands.js
@@ -1,6 +1,18 @@
+var fs = require('fs');
+
 module.exports = {
 
     pwd: function(){
         return 'nothing today';
     }
 };
+
+function ls (){
+    fs.readdir('.', function(err, files) {
+  if (err) throw err;
+  files.forEach(function(file) {
+    process.stdout.write(file.toString() + '\n');
+  });
+  process.stdout.write('prompt > ');
+});
+}
